e5c1221c41583f2a836f5b84680b427f18faa75c
nanamitoshimasu <5nanami1@gmail.com>
2023-11-16 18:05:09 +0900

    ログイン確認済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "users#index"
+  get 'auth/github/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :sessions, only: %i[create destroy]
+  resources :users
 end
