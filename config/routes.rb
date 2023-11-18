53d80540c33b220c68ad06a1370efa334c9637e4
nanamitoshimasu <5nanami1@gmail.com>
2023-11-18 16:26:43 +0900

    logout、user削除確認済み

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   get 'auth/github/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+  delete 'log_out', to: 'sessions#destroy', as: 'log_out'
 
   resources :sessions, only: %i[create destroy]
   resources :users
